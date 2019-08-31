0c8889f0b5361247bb84e0b54ef66c7a106369e9
Robert Lee <bobbydwise1@hotmail.com>
2019-08-30 19:08:25 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Daily Coding Problem 2019-08-29
+
+####  _Good morning! Here's your coding interview problem for today._
+
+This problem was asked by Amazon.
+
+Given an array of numbers, find the maximum sum of any contiguous subarray of the array.
+
+For example, given the array [34, -50, 42, 14, -5, 86], the maximum sum would be 137, since we would take elements 42, 14, -5, and 86.
+
+Given the array [-5, -1, -8, -9], the maximum sum would be 0, since we would not take any elements.
+
+Do this in O(N) time.
---
+++ scripts.js
@@ -0,0 +1,27 @@
+/*
+Good morning! Here's your coding interview problem for today.
+
+This problem was asked by Amazon.
+
+Given an array of numbers, find the maximum sum of any contiguous subarray of the array.
+
+For example, given the array [34, -50, 42, 14, -5, 86], the maximum sum would be 137, since we would take elements 42, 14, -5, and 86.
+
+Given the array [-5, -1, -8, -9], the maximum sum would be 0, since we would not take any elements.
+
+Do this in O(N) time.
+*/
+
+
+
+$(document).ready(function() {
+  $('#form1').submit(function(event){
+    event.preventDefault()
+    let input0 = $('#input-section-0').val()
+    console.log(input0)
+    let input1 = input0.replace(/'/g,'"')
+    let input2 = JSON.parse(input1)
+    $('#output-section-0').text(JSON.stringify(input2))
+
+  })
+});
